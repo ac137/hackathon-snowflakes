60eba374fdb665dff3eb3901e3c8dd3d79800bc4
alex c <apcabaj@gmail.com>
2016-07-09 16:29:24 -0400

    helper functions

---
+++ helper functions.py
@@ -0,0 +1,19 @@
+# snowflake thing
+
+def get_nearest_neightbours(arr, idx):
+	# index tuple of row, column
+	r,c = idx[0],idx[1]
+	n1, n2, n3 = None, None, None
+	try:
+		n1 = arr[r + 1,c]
+	except IndexError:
+		pass
+	try:
+		n2 = arr[r - 1, c]
+	except IndexError:
+		pass
+	try:
+		n3 = arr[r,c - 1]
+	except IndexError:
+		pass
+	return n1, n2, n3
