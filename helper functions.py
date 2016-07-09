4f749c45589ae4ee4907eb8f308caaa71be2f326
alex c <apcabaj@gmail.com>
2016-07-09 17:26:56 -0400

    add more functions

--- helper functions.py
+++ helper functions.py
@@ -1,6 +1,8 @@
 # snowflake thing
 
-def get_nearest_neightbours(arr, idx):
+from numpy import ones
+
+def get_nearest_neightbours_plane(arr, idx):
 	# index tuple of row, column
 	r,c = idx[0],idx[1]
 	n1, n2, n3 = None, None, None
@@ -17,3 +19,12 @@ def get_nearest_neightbours(arr, idx):
 	except IndexError:
 		pass
 	return n1, n2, n3
+
+#b is a parameter
+
+def initialize_array(side_len, b):
+	# initialize 3d array, for a background value b
+	arr = ones((side_len,side_len,side_len))*b
+	mid = side_len/2
+	arr[mid,mid,mid] = 1.0
+	return arr 
