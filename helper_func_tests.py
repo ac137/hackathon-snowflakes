cf1dfd4a3f7620556e0851563e1c847eab8d01cd
alex c <apcabaj@gmail.com>
2016-07-09 21:55:29 -0400

    FUCK INDEXING

--- helper_func_tests.py
+++ helper_func_tests.py
@@ -1,6 +1,7 @@
 # test stuff 
 
 import unittest
+from numpy import array, concatenate, insert
 from helper_functions import average_nearest 
 
 u = array([[[1,2,3,4],[9,8,6,4],[21,23,25,26],[3,2,4,3]],
@@ -8,4 +9,20 @@ u = array([[[1,2,3,4],[9,8,6,4],[21,23,25,26],[3,2,4,3]],
 	[[5,46,30,84],[1,8,3,2],[0,3,2,1],[4,8,0,2]],
 	[[9,6,1,5],[8,38,90,1],[3,3,3,3],[9,3,2,4]]])
 
-def test_average_1():
+mid_vals = array([[[2,5,3,1],[7,10.6,3,5.8],[4,10.8,14.4,8.4],[2,8,12,18]],
+[[5,46,30,84],[1,19.2,9.2,18.2],[0,9.8,2.2,5.2],[4,8,0,2]]])
+print mid_vals
+print mid_vals.shape
+print u[:,:,0].shape
+
+print u[0,:,:]
+print u[0,0,:]
+print u[0,:,0]
+
+#u_1 = insert(mid_vals,0,u[:,:,0],axis=2)
+#print u_1
+
+#def test_average_1():
+#	self.assertEqual(u_av, average_nearest(u))
+
+#print u
--- helper_functions.py
+++ helper_functions.py
@@ -32,22 +32,24 @@ def initialize_array(side_len, b):
 
 def average_nearest(arr):
 	# welcome to slicing hell
+	# INDEXING IS LAYER, ROW, COLUMN!!!
+
 	# sum of 1 column to the left, row above, and row below
-	u = arr[:-2,1:,1:-1] + arr[2:,1:,1:-1] + arr[1:-1,:-1,1:-1]
+	u = arr[1:-1,:-2,1:] + arr[1:-1,2:,1:] + arr[1:-1,1:-1,:-1]
 	# average alternating columns with layer above:
-	v = arr[1:-1,1::2,1:-1] # odd columns
-	w = arr[1:-1,2::2,1:-1] # even columns
+	v = arr[1:-1,1:-1,1::2] # odd columns
+	w = arr[1:-1,1:-1,2::2] # even columns
 	# add layer above to alternating rows
-	v[::2,:,:] += arr[1:-1:2,1::2,2:]
+	v[:,::2,:] += arr[2:,1:-1:2,1::2]
 	# add layer below to the other rows
-	v[1::2,:,:] += arr[2:-1:2,1::2,0:-2]
+	v[:,1::2,:] += arr[0:-2,2:-1:2,1::2]
 	# add layer below to the alternating rows
-	w[::2,:,:] += arr[1:-1:2,2::2,:-2]
+	w[:,::2,:] += arr[:-2,1:-1:2,2::2]
 	# add layer above to the other rows
-	w[1::2,:,:] += arr[2:-1:2,2::2,2:]
+	w[:,1::2,:] += arr[2:,2:-1:2,2::2]
 	# combine this whole ungodly mess
-	u[:,::2,1:-1] += v
-	u[:,1::2,1:-1] += w
+	u[1:-1,:,::2] += v
+	u[1:-1,:,1::2] += w
 	# divide by 5 to get average
 	u /= 5. #this syntax is glorious
 
@@ -57,11 +59,11 @@ def average_nearest(arr):
 	# ughghhh
 
 	# array sandwich - add top & bottom
-	u = concatenate(arr[1:-1,1:,0],u,arr[1:-1,1:,-1], axis=2)
+	u = concatenate(arr[0,1:-1,1:],u,arr[-1,1:-1,1:], axis=0)
 	# add row above and below, we have all the layers now
-	u = concatenate(arr[0,1:,:],u,arr[-1,1:,:], axis=0)
+	u = concatenate(arr[:,0,1:],u,arr[:,-1,1:], axis=1)
 	# add column to the left, have all layers and rows now
-	u = concatenate(arr[:,0,:],u)
+	u = concatenate(arr[:,:,0],u,axis=2)
 
 	# we are done, ugh
 	return u
